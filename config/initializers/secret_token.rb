270b176d6b22829d295e83f1340a57c1c6eb5d0e
Tony Baik <baiktony@yahoo.com>
2013-02-20 11:46:28 -0800

    Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebJamTeam8::Application.config.secret_token = '0c20d5d0fafab7a42179472e612218ef014fbac8c5cf773b3b0e7a38772a3c0b3274a1f779063a0e73581ba6a7c60923ab4f64aaeb952ecf64d9177195ae4ba9'
