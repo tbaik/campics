1250a0dfec042269684456ea809dd41ccebc71ce
Tony Baik <baiktony@yahoo.com>
2013-02-20 18:04:59 -0800

    Basic UI using bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 WebJamTeam8::Application.routes.draw do
+  get 'about' => 'pages#about'
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
