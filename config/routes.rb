77a667d9228cda2002e9bce8b0a77fc2855c24a5
Tony Baik <baiktony@yahoo.com>
2013-02-23 22:59:43 -0800

    small change

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ WebJamTeam8::Application.routes.draw do
   root :to => 'pages#home'
 
   get 'newpic' => 'pics#new'
-
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
