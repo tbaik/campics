7e88b8ba7a9b78a43256db980a44afc6182d2118
Tony Baik <baiktony@yahoo.com>
2013-02-26 02:27:55 -0800

    ui changes

--- app/assets/javascripts/viewport-sizing.js
+++ app/assets/javascripts/viewport-sizing.js
@@ -16,6 +16,7 @@
 
         var bottomBorderHeight = $('#bottom-border').height();
         var topHeaderHeight = 40;
+        var scrollHeight = $('.user-container').scrollTop();
 
         KokkoSuite.utils.Sizing.addFunction(function () {
             $('.vwrapper').css({
